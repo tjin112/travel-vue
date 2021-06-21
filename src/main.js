79e3cbe6f13d97213363115f44326d9e67d518cc
”tjin112” <940986798@qq.com>
2021-06-20 22:51:35 -0700

    commit

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-
+import './assets/styles/resets.css'
+import './assets/styles/border.css'
+import './assets/styles/iconfont.css'
 Vue.config.productionTip = false;
 
 new Vue({
---
+++ src/views/home/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+    <home-header />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HomeHeader from "@/components/home/Header.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HomeHeader,
+  },
+};
+</script>
