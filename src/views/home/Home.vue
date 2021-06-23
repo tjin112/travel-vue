83b1f1d984946e3ae48a279d8f63f3fec16df9d9
”tjin112” <940986798@qq.com>
2021-06-22 18:41:25 -0700

    home icons updated

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,6 +3,7 @@
     <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
     <home-header />
     <home-swiper />
+    <home-icons />
   </div>
 </template>
 
@@ -10,11 +11,13 @@
 // @ is an alias to /src
 import HomeHeader from "@/components/home/Header.vue";
 import HomeSwiper from "@/components/home/Swiper.vue"
+import HomeIcons from "@/components/home/Icons.vue"
 export default {
   name: "Home",
   components: {
     HomeHeader,
-    HomeSwiper
+    HomeSwiper,
+    HomeIcons
   },
 };
 </script>
